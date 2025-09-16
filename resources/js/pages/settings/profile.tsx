5aba566ed5d4c66d4dc2205b0f1e01f539383c14
mgionas <gionas@outlook.com>
2025-09-16 21:48:38 +0400

    Fix Auth issues

--- resources/js/pages/settings/profile.tsx
+++ resources/js/pages/settings/profile.tsx
@@ -1,5 +1,4 @@
 import ProfileController from '@/actions/App/Http/Controllers/Settings/ProfileController';
-import { send } from '@/routes/verification';
 import { type BreadcrumbItem, type SharedData } from '@/types';
 import { Transition } from '@headlessui/react';
 import { Form, Head, Link, usePage } from '@inertiajs/react';
@@ -73,27 +72,6 @@ export default function Profile({ mustVerifyEmail, status }: { mustVerifyEmail: 
 
                                     <InputError className="mt-2" message={errors.email} />
                                 </div>
-
-                                {mustVerifyEmail && auth.user.email_verified_at === null && (
-                                    <div>
-                                        <p className="-mt-4 text-sm text-muted-foreground">
-                                            Your email address is unverified.{' '}
-                                            <Link
-                                                href={send()}
-                                                as="button"
-                                                className="text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current! dark:decoration-neutral-500"
-                                            >
-                                                Click here to resend the verification email.
-                                            </Link>
-                                        </p>
-
-                                        {status === 'verification-link-sent' && (
-                                            <div className="mt-2 text-sm font-medium text-green-600">
-                                                A new verification link has been sent to your email address.
-                                            </div>
-                                        )}
-                                    </div>
-                                )}
 
                                 <div className="flex items-center gap-4">
                                     <Button disabled={processing}>Save</Button>
